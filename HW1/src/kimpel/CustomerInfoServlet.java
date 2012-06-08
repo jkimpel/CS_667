0b76ee04ecd15c09e0f267c52764339a15e6cc88
jkimpel <jkimpel@gmail.com>
2012-06-08 14:20:27 -0400

    Adding Project

---
+++ HW1/src/kimpel/CustomerInfoServlet.java
@@ -0,0 +1,92 @@
+package kimpel;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kimpel.beans.CustomerInfo;
+import org.apache.commons.beanutils.BeanUtils;
+
+/**
+ * Servlet implementation class CustomerInfoServlet
+ * 
+ * This servlet handles registration of a new customer
+ * 
+ * Joe Kimpel CS667
+ * 
+ * 6.4.2012
+ * 
+ */
+@WebServlet("/CustomerInfoServlet")
+public class CustomerInfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CustomerInfoServlet() {
+        super();
+    }
+
+	/**
+	 * This method simply calls doPost
+	 * 
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * This method receives a request which contains information for registering a new customer.
+	 * If the information is complete, it will return a confirmation page.
+	 * If the information is incomplete, it will return a page requesting the additional info.
+	 * 
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Initialize output
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		//Create the bean and attempt to populate
+		CustomerInfo cust = new CustomerInfo();
+		try {
+			BeanUtils.populate(cust, request.getParameterMap());
+		} catch (IllegalAccessException e) {
+			//We are checking the state of the bean after, so no action is necessary
+		} catch (InvocationTargetException e) {
+			//We are checking the state of the bean after, so no action is necessary
+		}
+		
+		//Check and display whether the user completed the form
+		boolean complete = cust.isComplete();
+		if(complete){
+			out.println(HTMLUtilities.COMPLETE_HEADER);
+		}else{
+			out.println(HTMLUtilities.INCOMPLETE_HEADER);
+			//If the info is not complete, we need to create a form
+			out.println(HTMLUtilities.FORM_OPENER);
+		}
+		
+		//Display entered information and prompts for what is still needed
+		out.println(HTMLUtilities.formEntry(cust.getCustomerID(), "Customer ID", "customerID", complete));
+		out.println(HTMLUtilities.formEntry(cust.getLastName(), "Last Name", "lastName", complete));
+		out.println(HTMLUtilities.formEntry(cust.getFirstName(), "First Name", "firstName", complete));
+		out.println(HTMLUtilities.formEntry(cust.getEmailAddress(), "Email Address", "emailAddress", complete));
+		
+		//Close up the page
+		if(!complete){
+			out.println(HTMLUtilities.FORM_CLOSER);
+		}
+		out.println(HTMLUtilities.FOOTER);
+		out.close();
+	}
+}
---
+++ HW1/src/kimpel/beans/CustomerInfo.java
@@ -0,0 +1,65 @@
+package kimpel.beans;
+
+import kimpel.HTMLUtilities;
+
+/* Simple bean that represents the information needed
+ * to register a new customer.
+ * 
+ * Joe Kimpel CS667
+ * 
+ * 6.4.2012
+ * 
+ */
+
+public class CustomerInfo{
+	private static final String DEFAULT_ID = "";
+	private static final String DEFAULT_LAST_NAME = "";
+	private static final String DEFAULT_FIRST_NAME = "";
+	private static final String DEFAULT_EMAIL = "";
+	
+	private String customerID = DEFAULT_ID;
+	private String lastName = DEFAULT_LAST_NAME;
+	private String firstName = DEFAULT_FIRST_NAME;
+	private String emailAddress = DEFAULT_EMAIL;
+
+	public String getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(String customerID) {
+		this.customerID = HTMLUtilities.filter(customerID);
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = HTMLUtilities.filter(lastName);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = HTMLUtilities.filter(firstName);
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = HTMLUtilities.filter(emailAddress);
+	}
+	
+	//If all of the fields are non-default, the object has been completely filled
+	public boolean isComplete(){
+		if (!getCustomerID().equals(DEFAULT_ID) && !getLastName().equals(DEFAULT_LAST_NAME)
+				&& !getFirstName().equals(DEFAULT_FIRST_NAME) && !getEmailAddress().equals(DEFAULT_EMAIL))		
+			return true;
+		else
+			return false;
+	}	
+}
